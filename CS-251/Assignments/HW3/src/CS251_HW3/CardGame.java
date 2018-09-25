a96a40fc00e6a157e703d834b9fd20cbe30778c2
Shoobs <Shoobs@DESKTOP-1TJAMQC>
2018-09-25 18:29:27 -0500

    HW4_Files

---
+++ CS-251/Assignments/HW3/src/CS251_HW3/CardGame.java
@@ -0,0 +1,11 @@
+package CS251_HW3;
+import java.util.Scanner;
+
+public class CardGame {
+
+	private static final String[] cardNumbers = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+	private static final String[] cardSuits = {"D","H","C","S"};
+
+	//TODO
+	
+}
---
+++ CS-251/Assignments/HW3/src/CS251_HW3/CardGameDriver.java
@@ -0,0 +1,11 @@
+package CS251_HW3;
+
+public class CardGameDriver {
+
+	public static void main(String[] args) {
+
+		//TODO
+
+	}
+
+}
