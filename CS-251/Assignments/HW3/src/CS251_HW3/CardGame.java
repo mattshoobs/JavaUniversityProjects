3ae0e453dfb534ecc2e3882d83206321a5e38e93
Shoobs <Shoobs@DESKTOP-1TJAMQC>
2018-09-30 19:56:33 -0500

    HW3 Final Formatted

--- CS-251/Assignments/HW3/src/CS251_HW3/CardGame.java
+++ CS-251/Assignments/HW3/src/CS251_HW3/CardGame.java
@@ -1,113 +1,105 @@
 package CS251_HW3;
+
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CardGame {
 
-	private static final String[] cardNumbers = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
-	private static final String[] cardSuits = {"D","H","C","S"};
+	private static final String[] cardNumbers = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+	private static final String[] cardSuits = { "D", "H", "C", "S" };
 	private static final int defaultPlayers = 2;
-	
+
 	private boolean deck[];
 	private String[][] players;
 	private int cardsDealt;
 	private boolean hasDealtCards;
 
-	
-	public CardGame (int totalPlayers) {
-		
+	public CardGame(int totalPlayers) {
+
 		totalPlayers = totalPlayers <= 8 && totalPlayers >= 2 ? totalPlayers : defaultPlayers;
-		this.players = new String [totalPlayers][5];
+		this.players = new String[totalPlayers][5];
 		this.deck = new boolean[52];
 		this.resetDeck();
 	}
-	
+
 	public int getTotalCardsDealt() {
 		return cardsDealt;
 	}
-	
-	
+
 	private void setHasDealtCards(boolean hasDealtCards) {
 		this.hasDealtCards = hasDealtCards;
 	}
-	
+
 	public boolean getHasDealtCards() {
 		return hasDealtCards;
 	}
-	
+
 	private void setCardsDealt(int cardsDealt) {
 		this.cardsDealt = cardsDealt;
 	}
 
+	public void resetDeck() {
 
-	public void resetDeck(){	
-		
 		Arrays.fill(this.deck, Boolean.TRUE);
 		this.setHasDealtCards(false);
 		this.setCardsDealt(0);
-		
+
 	}
-	
-	public void dealHands(){
-		
+
+	public void dealHands() {
+
 		if (!getHasDealtCards()) {
-			for(int i = 0; i<this.players.length; i++){
-				for(int k = 0; k<this.players[i].length; k++){
-					 players[i][k] = dealCard();	
+			for (int i = 0; i < this.players.length; i++) {
+				for (int k = 0; k < this.players[i].length; k++) {
+					players[i][k] = dealCard();
 				}
-			}	
-		this.setHasDealtCards(true);
-		}	
-		else {
+			}
+			this.setHasDealtCards(true);
+		} else {
 			System.out.println("Error - The cards have already been dealt. Can't deal more cards");
 		}
 	}
 
-	
-	private String dealCard(){
-        
-        while(true){
-            int tempCard = ThreadLocalRandom.current().nextInt(0, 52);
-            if(deck[tempCard]) {
-                deck[tempCard] = false;
-                return convertCard(tempCard);
-            }
-        }
-    }
+	private String dealCard() {
 
-	
-	private String convertCard(int card){
-		return cardSuits[card%4] + cardNumbers[card%13];
+		while (true) {
+			int tempCard = ThreadLocalRandom.current().nextInt(0, 52);
+			if (deck[tempCard]) {
+				deck[tempCard] = false;
+				return convertCard(tempCard);
+			}
+		}
 	}
 
+	private String convertCard(int card) {
+		return cardSuits[card % 4] + cardNumbers[card % 13];
+	}
 
-	public void displayHands(){
+	public void displayHands() {
 
 		String output = "";
-		for(int i = 0; i<this.players.length; i++){
-			output += String.format("\nPlayer %s hand: ", i+1); 
-		    for( String card : this.players[i]) {
-		        output += card + " ";
-		    }
-		    output += "\n";
+		for (int i = 0; i < this.players.length; i++) {
+			output += String.format("\nPlayer %s hand: ", i + 1);
+			for (String card : this.players[i]) {
+				output += card + " ";
+			}
+			output += "\n";
 		}
 		System.out.print(output);
 	}
-	
+
 	public void addPlayers(int playersToAdd) {
 		if (playersToAdd < 0 || this.players.length + playersToAdd > 8) {
 			System.out.print("Error - Can't add negative players or exceed 8 total players.");
 			return;
 		}
-		//this.newPlayers = new String [playersToAdd][5];
-		players = Arrays.copyOf(this.players,this.players.length + playersToAdd);
-		for(int i = this.players.length - (playersToAdd); i < players.length ; i++){
+		players = Arrays.copyOf(this.players, this.players.length + playersToAdd);
+		for (int i = this.players.length - (playersToAdd); i < players.length; i++) {
 			this.players[i] = new String[5];
-			for(int k = 0; k<players[i].length; k++){
-				 this.players[i][k] = dealCard();
+			for (int k = 0; k < players[i].length; k++) {
+				this.players[i][k] = dealCard();
 			}
 		}
 	}
-	
-	
+
 }
--- CS-251/Assignments/HW3/src/CS251_HW3/CardGameDriver.java
+++ CS-251/Assignments/HW3/src/CS251_HW3/CardGameDriver.java
@@ -4,25 +4,25 @@ public class CardGameDriver {
 
 	public static void main(String[] args) {
 
-		for(int i = -1; i < 10; i++) {	
+		for (int i = -1; i < 10; i++) {
 			CardGame g1 = new CardGame(i);
 			System.out.print("________________________________________");
-			g1.resetDeck(); //make it so all cards available
-			g1.dealHands(); //deal out hands to each player
+			g1.resetDeck(); // make it so all cards available
+			g1.dealHands(); // deal out hands to each player
 			g1.displayHands();
-			g1.dealHands(); //can not do this, will print error message
+			g1.dealHands(); // can not do this, will print error message
 			g1.addPlayers(1);
 			g1.displayHands();
-		}	
-		
+		}
+
 		CardGame g2 = new CardGame(3);
 		System.out.print("________________________________________");
-		g2.resetDeck(); //make it so all cards available
-		g2.dealHands(); //deal out hands to each player
+		g2.resetDeck(); // make it so all cards available
+		g2.dealHands(); // deal out hands to each player
 		g2.displayHands();
-		g2.dealHands(); //can not do this, will print error message
+		g2.dealHands(); // can not do this, will print error message
 		g2.addPlayers(-1);
 		g2.displayHands();
-		
+
 	}
 }
