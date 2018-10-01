739d4c3d5bf5ebd6aa7885e8c8f7f8091839deda
Shoobs <Shoobs@DESKTOP-1TJAMQC>
2018-09-30 19:54:20 -0500

    HW3 Final

--- CS-251/Assignments/HW3/src/CS251_HW3/CardGame.java
+++ CS-251/Assignments/HW3/src/CS251_HW3/CardGame.java
@@ -1,6 +1,5 @@
 package CS251_HW3;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CardGame {
@@ -13,13 +12,14 @@ public class CardGame {
 	private String[][] players;
 	private int cardsDealt;
 	private boolean hasDealtCards;
+
 	
 	public CardGame (int totalPlayers) {
 		
 		totalPlayers = totalPlayers <= 8 && totalPlayers >= 2 ? totalPlayers : defaultPlayers;
 		this.players = new String [totalPlayers][5];
 		this.deck = new boolean[52];
-		this.resetDeck(this.deck);
+		this.resetDeck();
 	}
 	
 	public int getTotalCardsDealt() {
@@ -40,30 +40,31 @@ public class CardGame {
 	}
 
 
-	public void resetDeck(boolean[] deck){	
-		Arrays.fill(deck, Boolean.TRUE);
+	public void resetDeck(){	
+		
+		Arrays.fill(this.deck, Boolean.TRUE);
 		this.setHasDealtCards(false);
 		this.setCardsDealt(0);
 		
 	}
 	
-	public void dealHands(String[][] players, boolean[] deck){
+	public void dealHands(){
 		
 		if (!getHasDealtCards()) {
-			for(int i = 0; i<players.length; i++){
-				for(int k = 0; k<players[i].length; k++){
-					 players[i][k] = dealCard(deck);	
+			for(int i = 0; i<this.players.length; i++){
+				for(int k = 0; k<this.players[i].length; k++){
+					 players[i][k] = dealCard();	
 				}
 			}	
+		this.setHasDealtCards(true);
 		}	
 		else {
 			System.out.println("Error - The cards have already been dealt. Can't deal more cards");
 		}
-			
 	}
 
 	
-	private String dealCard(boolean[] deck){
+	private String dealCard(){
         
         while(true){
             int tempCard = ThreadLocalRandom.current().nextInt(0, 52);
@@ -72,22 +73,20 @@ public class CardGame {
                 return convertCard(tempCard);
             }
         }
-        
     }
 
 	
-	public String convertCard(int card){
-		
+	private String convertCard(int card){
 		return cardSuits[card%4] + cardNumbers[card%13];
 	}
 
 
-	public void displayHands(String[][] players){
+	public void displayHands(){
 
 		String output = "";
-		for(int i = 0; i<players.length; i++){
+		for(int i = 0; i<this.players.length; i++){
 			output += String.format("\nPlayer %s hand: ", i+1); 
-		    for( String card : players[i]) {
+		    for( String card : this.players[i]) {
 		        output += card + " ";
 		    }
 		    output += "\n";
@@ -95,6 +94,20 @@ public class CardGame {
 		System.out.print(output);
 	}
 	
+	public void addPlayers(int playersToAdd) {
+		if (playersToAdd < 0 || this.players.length + playersToAdd > 8) {
+			System.out.print("Error - Can't add negative players or exceed 8 total players.");
+			return;
+		}
+		//this.newPlayers = new String [playersToAdd][5];
+		players = Arrays.copyOf(this.players,this.players.length + playersToAdd);
+		for(int i = this.players.length - (playersToAdd); i < players.length ; i++){
+			this.players[i] = new String[5];
+			for(int k = 0; k<players[i].length; k++){
+				 this.players[i][k] = dealCard();
+			}
+		}
+	}
 	
 	
 }
--- CS-251/Assignments/HW3/src/CS251_HW3/CardGameDriver.java
+++ CS-251/Assignments/HW3/src/CS251_HW3/CardGameDriver.java
@@ -4,16 +4,25 @@ public class CardGameDriver {
 
 	public static void main(String[] args) {
 
-		CardGame g1 = new CardGame(4);
-		CardGame g2 = new CardGame(8);
-		CardGame g3 = new CardGame(10);
+		for(int i = -1; i < 10; i++) {	
+			CardGame g1 = new CardGame(i);
+			System.out.print("________________________________________");
+			g1.resetDeck(); //make it so all cards available
+			g1.dealHands(); //deal out hands to each player
+			g1.displayHands();
+			g1.dealHands(); //can not do this, will print error message
+			g1.addPlayers(1);
+			g1.displayHands();
+		}	
 		
-		g1.displayHands(players);
-		
-		
-		
-		
+		CardGame g2 = new CardGame(3);
+		System.out.print("________________________________________");
+		g2.resetDeck(); //make it so all cards available
+		g2.dealHands(); //deal out hands to each player
+		g2.displayHands();
+		g2.dealHands(); //can not do this, will print error message
+		g2.addPlayers(-1);
+		g2.displayHands();
 		
 	}
-
 }
