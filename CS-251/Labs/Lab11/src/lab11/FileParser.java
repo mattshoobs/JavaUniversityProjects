0293381a69e69f5b1a81071d3df25fa848a48ff7
Matthew Schubring <matthew.schubring@gmail.com>
2018-11-26 13:27:12 -0600

    Lab11_Complete

--- CS-251/Labs/Lab11/src/lab11/FileParser.java
+++ CS-251/Labs/Lab11/src/lab11/FileParser.java
@@ -1,8 +1,32 @@
 package lab11;
 
 public class FileParser {
-
+	private String inLine;
 	
+	public FileParser (String inLine) {
+		this.inLine = inLine;
+		
+	}
+
+	public String parse() {
+		
+		String[] tmp = inLine.split(":");
+		
+		String line = tmp[0];
+		
+		int index1 = Integer.parseInt(tmp[1]);
+		int index2 = Integer.parseInt(tmp[2]);
+		
+		if (index1 > index2) {
+			return "i > j, invalid indicies.";
+		}
+		else if (index1 > line.length() || index2 > line.length()) {
+			return "i or j are larger than string length";
+		}
+		line = line.substring(index1, index2);
+		return line;
+		
+	}
 	
 	
 	
--- CS-251/Labs/Lab11/src/lab11/FileParserDriver.java
+++ CS-251/Labs/Lab11/src/lab11/FileParserDriver.java
@@ -1,12 +1,40 @@
 package lab11;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import lab11.FileParser;
+
+
 public class FileParserDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		Scanner prompt = new Scanner(System.in);
+		
+		System.out.println("What is the name of the input file?");
+		String inName = prompt.nextLine();
+		
+		System.out.println("What is the name of the output file?");
+		String outName = prompt.nextLine();
 
+		try(Scanner scr = new Scanner(new File("./docs/" + inName));
+			PrintWriter pw = new PrintWriter("./docs/" + outName)) {
+			while(scr.hasNextLine()) {
+				String inLine = scr.nextLine();
+				FileParser f = new FileParser(inLine);
+				pw.println(f.parse());
+			}
+			System.out.print("File writing complete! \nThe output file is located in \"./docs/" + outName + "\"");
+		} 
+		catch (FileNotFoundException e) {
+			System.out.print("File Not Found");
+		}
 		
-		
+		prompt.close();
 	}
 
 }
